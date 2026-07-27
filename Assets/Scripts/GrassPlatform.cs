253b0c69dfdff77da7a8063a842feb7ff908563a
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2026-07-27 21:17:41 +0700

    grass platform

---
+++ Assets/Scripts/GrassPlatform.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GrassPlatform : MonoBehaviour
+{
+
+    [SerializeField] private Transform _grassLeft;
+    [SerializeField] private Transform _grassRight;
+
+    [SerializeField, Range(0, 10)] private int _grassMidSize = 0;
+
+    private bool _isDirty = false;
+
+
+    private void OnValidate()
+    {
+        _isDirty = true;
+    }
+    private void Update()
+    {
+        if (_isDirty)
+        {
+            RebuildLayout();
+            _isDirty = false;
+        }
+    }
+
+    private void RebuildLayout()
+    {
+        float startPosX = 0.5f;
+        SpriteRenderer grassMidSpriteRenderder = GetComponent<SpriteRenderer>();
+
+        grassMidSpriteRenderder.size = new Vector2(_grassMidSize, grassMidSpriteRenderder.size.y);
+
+        float offset = _grassMidSize * 0.5f;
+
+        // move the left and right grass to the correct position
+        if (_grassLeft != null && _grassRight != null)
+        {
+            _grassLeft.localPosition = new Vector3(-startPosX - offset, 0, 0);
+            _grassRight.localPosition = new Vector3(startPosX + offset, 0, 0);
+        }
+
+
+
+
+
+    }
+}
